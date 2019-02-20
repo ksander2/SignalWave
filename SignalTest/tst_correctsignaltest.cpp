d2b51c25081b034d101b7d4b79c4d6a45319395c
ksander2 <an_sobolev90@mail.ru>
2019-02-20 15:25:50 +0700

    correct test

--- SignalTest/tst_correctsignaltest.cpp
+++ SignalTest/tst_correctsignaltest.cpp
@@ -9,30 +9,33 @@ public:
     correctSignalTest();
     ~correctSignalTest();
 
+private:
+    ISignal  *testSineSignal;
+    int countSamples;
+
 private slots:
     void test_case1();
 
 };
 
-correctSignalTest::correctSignalTest()
+correctSignalTest::correctSignalTest(): countSamples(256)
 {
-
+    testSineSignal = new SineSignal(50, 1, countSamples);
 }
 
 correctSignalTest::~correctSignalTest()
 {
-
+    delete testSineSignal;
 }
 
 void correctSignalTest::test_case1()
 {
-   ISignal  *pp = new SineSignal(50, 1, 256);
-   pp->computeSignal();
-   // testSineSignal.computeSignal();
-   // auto vectorSignal = testSineSignal.getVector();
-   // std::complex<double> *complexArray = vectorSignal.data();
-    //QCOMPARE(complexArray[0].real(), 1);
-    //QVERIFY(condition)
+   testSineSignal->computeSignal();
+   std::vector<std::complex<double>> vectorSignal = testSineSignal->getVector();
+   std::complex<double> complexValue = vectorSignal[0];
+   QCOMPARE(complexValue.real(), 0);
+   QCOMPARE(vectorSignal.size(), countSamples);
+   //QVERIFY(condition)
 }
 
 QTEST_APPLESS_MAIN(correctSignalTest)
